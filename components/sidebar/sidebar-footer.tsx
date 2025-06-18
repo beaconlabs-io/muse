6b2ad6df69f8baf954b35d8557bdcd9c073aaf0d
shuhei tanaka <shuhei46491@gmail.com>
2025-06-18 19:02:08 +0900

    feat: add SidebarFooterComponent with dropdown links to Beacon Labs website and GitHub

---
+++ components/sidebar/sidebar-footer.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Link from "next/link";
+import { Building, ChevronUp, SquareArrowOutUpRight } from "lucide-react";
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarFooter,
+} from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+export function SidebarFooterComponent() {
+  return (
+    <SidebarFooter>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton>
+                <Building /> Beacon Labs
+                <ChevronUp className="ml-auto" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent side="top" className="w-48">
+              <DropdownMenuItem>
+                <Link
+                  href="https://beaconlabs.io"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="flex items-center gap-2">
+                    <span>Website</span>
+                    <SquareArrowOutUpRight />
+                  </div>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link
+                  href="https://github.com/beaconlabs-io"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="flex items-center gap-2">
+                    <span>Github</span>
+                    <SquareArrowOutUpRight />
+                  </div>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarFooter>
+  );
+}
