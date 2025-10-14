894812224de2e7744fe0d8685cceabcdaaef543c
shuhei tanaka <shuhei46491@gmail.com>
2025-10-14 16:29:02 +0900

    fix: update environment variable check for chain configuration in wagmi.ts

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -4,6 +4,6 @@ import { baseSepolia, celo, optimism } from "wagmi/chains";
 export const config = getDefaultConfig({
   appName: "Muse - Logic Model Builder",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-  chains: process.env.NEXT_PUBLIC_ENV === "development" ? [baseSepolia] : [optimism, celo],
+  chains: process.env.NODE_ENV === "development" ? [baseSepolia] : [optimism, celo],
   ssr: true,
 });
